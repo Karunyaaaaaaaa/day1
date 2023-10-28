95264b1f9a2855430b8645827722a402169e1f31
Karunyaaaaaaaa <139757927+Karunyaaaaaaaa@users.noreply.github.com>
2023-10-28 12:21:44 +0530

    Add files via upload

---
+++ pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "../styles/About.css";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="aboutTop"></div>
+      <div className="aboutBottom">
+      <h1> ABOUT US</h1>
+      <p>
+        
+      </p>
+    </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="home" >
+      <div className="headerContainer" >
+        <h1>Domestic Waste Management</h1>
+        <p>Domestic waste is any waste that is produced in the home environment. </p>
+        
+        </div>
+    </div>
+  );
+  
+  }
+
+export default Home;
